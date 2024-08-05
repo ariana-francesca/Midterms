31e65e1a735d4685e1d10827fe6055790f68e6a9
Ariana <anpunsalang@student.hau.edu.ph>
2024-08-05 15:25:54 +0800

    Update server_routing.js

--- server_routing.js
+++ server_routing.js
@@ -1,13 +1,36 @@
-var express = require("express");
+// Basic Routing
+
+var express = require('express');
 var app = express();
 
-app.get("/", function(req, res){
-    res.send("You have successfully created your second app!");
+app.get('/', function (req, res) {
+    console.log("There is GET request for the homepage!");
+    res.send('Here is the GET Method!');
 })
 
-var server = app.listen(2001, function () {
+app.post('/', function (req, res) {
+    console.log("A POST request for the homapage is accessed!");
+    res.send('Here is the POST Method!');
+})
+
+app.get('/list_user', function (req, res) {
+    console.log("Got a GET request for /list_user");
+    res.send('This is Page Listing');
+})
+
+app.get('/ab*cd', function (req, res) {
+    console.log("Got a GET request for /ab*cd");
+    res.send('Pattern Match Page');
+})
+
+app.get('/name', function (req, res){
+    console.log("Got a GET request for /name")
+    res.send('Ariana Francesca Punsalang')
+})
+
+var server = app.listen(4000, function () {
     var host = server.address().address
     var port = server.address().port
-
+    
     console.log("Example app listening at http://%s:%s", host, port)
 })
